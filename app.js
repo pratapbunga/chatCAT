c8cbdf946c81b38515f59876d995767ca2ac6bcf
pratap bunga <pratap234337@gmail.com>
2015-10-14 10:11:37 -0700

    Added auth folder and configured PassportJS

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var express = require('express'),
 	session = require('express-session'),
 	config = require('./config/config.js'),
 	env = process.env.NODE_ENV || 'development',
-	connectMongo = require('connect-mongo')(session);
+	connectMongo = require('connect-mongo')(session),
+	mongoose = require('mongoose').connect(config.dbUrl),
+	passport = require('passport'),
+	FacebookStrategy = require('passport-facebook').Strategy,
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
@@ -29,7 +32,25 @@ if(env === 'development'){
 	));
 }
 
+var userSchema = mongoose.Schema({
+	username:String,
+	password:String,
+	fullname:String
+})
 
+var Person = mongoose.model('users', userSchema);
+
+var John = new Person({
+	username:'pratap234337',
+	password:'thisismypassword',
+	fullname:'pratap bunga'
+})
+
+John.save(function(err){
+	console.log('Done!');
+})
+
+require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose);
 //Load the module like this in other files
 require('./routes/routes.js')(express, app);
 
